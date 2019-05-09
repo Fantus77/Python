01b8e8cf7c392f8d0e5d3dd1d648364a8f782956
Jay Embry <jembry@hardsuitlabs.com>
2019-05-09 14:03:54 -0700

    Updating tree with new plan

--- tree.py
+++ tree.py
@@ -22,31 +22,43 @@ def tree(levels):
 
 	branches(levels, fmove / 2)
 """
-	
-def branches(levels, fmove):
-	if levels >= 2:
-		tt.left(45)
-		tt.forward(fmove)
-		if levels == 3:
-			branches(levels, fmove / 2)
-		tt.forward(-fmove)
-		tt.right(90)
-		tt.forward(fmove)
-		if levels == 3:
-			branches(levels, fmove / 2)
-		tt.forward(-fmove)
-	
-def tree(levels):
-	start = 90
-	fmove = 100
-	tt.left(start)
-	tt.forward(fmove)
+def branches(fmove):
+	if counter == 1:
+		
 
-	branches(levels, fmove / 2).
-	
-	
-	
-	
-	
+
+
+def tree(levels):
+	counter = 0
+	halfturn = 45
+	fullturn = 90
+	fmove = 100
+	tt.left(fullturn)
+	tt.forward(fmove)
+	if counter < levels:
+		branches(fmove)
+		counter = counter + 1
+		tree()
+	elif counter == levels:
+		branches(fmove)
+
+
+
+
+
+
+"""
+First function called should set up a counter that increments every time it is run until the value of the counter matches the value of 'levels'.
+
+But what do I do in the case of when the counter == levels? Should it run or stop there? I guess it should run and then NOT increment. How to do that?
+
+Function that is called from the first function should make the branches. The math to do this might just destroy my brain, cause I hate math.
+
+EZ
+"""
+
+
+
+
 tree(3)
 input()
