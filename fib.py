50d88b0da174ccd2196207e0a05db5056af35073
Fantus <jay.embry@gmail.com>
2019-05-11 10:29:55 -0700

    more work on fib and working to get fibSum working

--- fib.py
+++ fib.py
@@ -1,4 +1,4 @@
-
+"""
 def fib(X):
     #N = (X - 1) + (X - 2)
     if X == 1:
@@ -12,24 +12,24 @@ def fib(X):
 print(fib(1) == 1)
 print(fib(2) == 1)
 print(fib(6) == 8)
-
 """
+#This doesn't work...need to figure out proper formula still
 def fibSum(Y):
-    #N = ??
-    if X == 1:
+    #N = fibSum(??) ?+? fibSum(??)
+    if Y == 1:
         return 1
-    if X == 2:
-        return 1
-    if X >= 3:
-        return 
-"""
+    if Y == 3:
+        return 4
+    if Y >= 3:
+        return fibSum(Y - 1) + fibSum(Y - 2) #Is it the same as fib? I think it has to do more with adding positions together...but how to describe that in the formula?
 
-"""
+
+
 # EXPECTED VALUES - TEST CASES FOR fibSum
 print(fibSum(1) == 1) # 1
 print(fibSum(3) == 4) # 1 + 1 + 2
 print(fibSum(8) == 54) # 1 + 1 + 2 + 3 + 5 + 8 + 13 + 21
-"""
 
-fib(6)
-#fibSum()
+
+#fib(6)
+fibSum(35)
