34418f244c320c540eaab4cabe869d7ad2bc4051
Fantus <jay.embry@gmail.com>
2019-05-11 02:23:39 -0700

    adding function fibSum and reorganize everything a bit

--- fib.py
+++ fib.py
@@ -1,3 +1,7 @@
+# EXPECTED VALUES - TEST CASES FOR fib
+print(fib(1) == 1)
+print(fib(2) == 1)
+print(fib(6) == 8)
 
 def fib(X):
     #N = (X - 1) + (X - 2)
@@ -8,15 +12,22 @@ def fib(X):
     if X >= 3:
         return fib(X - 1) + fib(X - 2)
 
-
-# EXPECTED VALUES - TEST CASES
-print(fib(1) == 1)
-print(fib(2) == 1)
-print(fib(6) == 8)
-
 """
+# EXPECTED VALUES - TEST CASES FOR fibSum
 print(fibSum(1) == 1) # 1
 print(fibSum(3) == 4) # 1 + 1 + 2
 print(fibSum(8) == 54) # 1 + 1 + 2 + 3 + 5 + 8 + 13 + 21
 """
+
+"""
+def fibSum(Y):
+    #N = ??
+    if X == 1:
+        return 1
+    if X == 2:
+        return 1
+    if X >= 3:
+        return 
+"""
 fib(6)
+#fibSum()
