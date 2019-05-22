96cb557c61dc4c568081c7934ed6aa7419032f88
Fantus <jay.embry@gmail.com>
2019-05-21 23:45:10 -0700

    Progress in converting from figuring out how many months it would take to save the down payment to figuring out what percentage of the monthly salary needs to be put aside to save up the down payment in a set amount of months

--- ps1/ps1c.py
+++ ps1/ps1c.py
@@ -5,16 +5,17 @@ def first(Annual_Salary, Total_Cost, Semi_Annual_Raise):
     Return_On_Investment = 0.04
 # figuring out how much the down payment should be based on the total cost
     Down_Payment_Amount = Total_Cost * Portion_Down_Payment
-    Down_Payment_Amount = int(Down_Payment_Amount)
+    # Down_Payment_Amount = int(Down_Payment_Amount)
 # adding to Current_Savings until it is >= Down_Payment_Amount
     Current_Savings = 0
     Months = 0
     while Current_Savings <= Down_Payment_Amount:
-        Current_Savings = Current_Savings + ((Monthly_Salary * Portion_Saved) + (Current_Savings * Return_On_Investment / 12))
+        Current_Savings = Current_Savings 
+        # Current_Savings = Current_Savings + ((Monthly_Salary) + (Current_Savings * Return_On_Investment / 12))
         Months = Months + 1
-    #     if DivisibleBySix(Months):
-    #         Annual_Salary = (Annual_Salary * Semi_Annual_Raise) + Annual_Salary
-    #         Monthly_Salary = Annual_Salary / 12
+        if DivisibleBySix(Months):
+            Annual_Salary = (Annual_Salary * Semi_Annual_Raise) + Annual_Salary
+            Monthly_Salary = Annual_Salary / 12
 
     # print("The total amount of months to save:", Months)
 
@@ -30,8 +31,8 @@ def runme():
     Annual_Salary = int(input("Please enter annual salary:"))
     # Portion_Saved = float(input("Please enter percentage of monthly salary to save:"))
     # Total_Cost = int(input("Please enter the total cost of dream home:"))
-    Semi_Annual_Raise = float(input("Please enter your annual raise percentage:"))
-    first(Annual_Salary, 1000000, Semi_Annual_Raise)
+    # Semi_Annual_Raise = float(input("Please enter your annual raise percentage:"))
+    first(Annual_Salary, 1000000, 0.07)
 
 
 runme()
