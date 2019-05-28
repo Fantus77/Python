8061895de5defaf49d64353f5496cbca81254b4c
Fantus <jay.embry@gmail.com>
2019-05-28 16:28:34 -0700

    Made this work by figuring out that the value of the length of the list exceeds the last index reference (100 > 99)

--- ps1/listtest.py
+++ ps1/listtest.py
@@ -3,11 +3,11 @@ This is where I start to mess around with lists
 """
 
 lst = list(range(1, 101))
-val = 14
-def FindBest(lst, val):
+# val = 14
+def FindBest(lst):
     lstlen = len(lst)
-    fun = lst[lstlen]
+    fun = lst[lstlen - 1]
     print(fun)
 
 
-FindBest(lst, val)
+FindBest(lst)
